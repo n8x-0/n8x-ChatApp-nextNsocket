af2bd46e14c0a8fc596924674a81e685e3dc8fbc
shayan <syedshayan1324@gmail.com>
2024-08-26 16:23:01 +0500

    chats feature added

--- src/components/clientsideHeader/clientsideHeader.tsx
+++ src/components/clientsideHeader/clientsideHeader.tsx
@@ -3,7 +3,7 @@ type Props = { children: React.ReactNode }
 const ClientSideHeader = ({ children }: Props) => {
     return (
         <header className="w-full md:px-12 px-5 md:py-5 py-3 flex justify-between items-center fixed top-0 bg-zinc-800 border-b-[1px] border-zinc-700">
-            <div className="text-3xl font-medium">n8x</div>
+            <div className="text-3xl font-medium pr-3">n8x</div>
             <div className='flex md:gap-4 gap-2 font-medium items-center'>
                 {children}
             </div>
